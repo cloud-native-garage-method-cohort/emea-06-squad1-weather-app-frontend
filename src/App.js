2551a6063bf300b6ccf8956cac66101dd6cc0d83
ChimiDEV <Tim.Walter1996@web.de>
2021-10-07 16:40:27 +0200

    Initial working frontend

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React, { useEffect, useState } from 'react';
+
+const App = () => {
+  const [weather, setWeather] = useState({});
+
+  useEffect(() => {
+    async function fetchWeather() {
+      let response = await (
+        await fetch('http://localhost:3000/weather?city=London')
+      ).json();
+
+      console.log(response);
+      setWeather(response);
+    }
+
+    fetchWeather();
+  }, []);
+
+  return <div>Weather: {JSON.stringify(weather)}</div>;
+};
+
+export default App;
